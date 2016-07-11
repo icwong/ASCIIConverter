1a9553daa033f78279f734bb6b367d76181dc581
icwong <icwong@sfu.ca>
2016-07-11 23:29:36 +0000

    still

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'compare' => 'main#compare'
   get 'mail' => 'authentications#mail'
   get 'verify' => 'authentications#verify'
+  post 'user_registration' => "main#index"
   
 
   resources :posts
